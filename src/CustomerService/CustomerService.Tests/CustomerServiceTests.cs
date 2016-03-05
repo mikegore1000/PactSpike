4f1468ae58866882ab01ac3c0dd6dc12ba12285f
Mike Gore <mikegore1000@msn.com>
2016-03-05 08:50:21 +0000

    Ensured service honours the .NET client pacts.

---
+++ src/CustomerService/CustomerService.Tests/CustomerServiceTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Owin.Testing;
+using NUnit.Framework;
+using PactNet;
+
+namespace CustomerService.Tests
+{
+    public class CustomerServiceTests
+    {
+        [Test]
+        public void EnsureApiHonoursClrClientPact()
+        {
+            var pactVerifier = new PactVerifier(
+                setUp: () => { },
+                tearDown: () => { });
+
+            pactVerifier.ProviderState("A customer with id '123'");
+
+            using (var testServer = TestServer.Create<CustomerServiceStartup>())
+            {
+                pactVerifier
+                    .ServiceProvider("Customer Service API", testServer.HttpClient)
+                    .HonoursPactWith("CLR Client")
+                    .PactUri("../../../../pacts/clr_client-customer_api.json")
+                    .Verify();
+            }
+        }
+    }
+}
---
+++ src/CustomerService/CustomerService/App_Start/CustomerServiceStartup.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Http;
+using CustomerService;
+using Microsoft.Owin;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Owin;
+
+[assembly:OwinStartup(typeof(CustomerServiceStartup))]
+
+namespace CustomerService
+{
+    public class CustomerServiceStartup
+    {
+        public void Configuration(IAppBuilder builder)
+        {
+            var config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            builder.UseWebApi(config);
+        }
+    }
+}
